86db4a3317a995a8bda17b37dc6f03ccf5c835fc
Ritisha12 <68296139+Ritisha12@users.noreply.github.com>
2020-08-28 14:06:00 -0400

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,30 @@
+class Slingshot{
+    constructor(ball,string){
+        var options = {
+            bodyA: ball,
+            pointB: string,
+            stiffness: 0.04,
+            length: 10
+        }
+        
+        this.pointB = string
+        this.slingshot = Constraint.create(options);
+        World.add(world, this.slingshot);
+    }
+    fly(){
+        this.slingshot.bodyA=null
+    } 
+    
+    display(){
+       
+        if(this.slingshot.bodyA){
+            var posA = this.slingshot.bodyA.position;
+            var posB = this.pointB;
+            strokeWeight(4);
+           // stroke(48,22,8)
+           line(posA.x,posA.y,posB.x,posB.y)
+        }
+    
+    }
+  
+}
